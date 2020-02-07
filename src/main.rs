6fb37fa4816d88234ccff193d6a26a567362ded0
ostrosco <ostrosco@fastmail.fm>
2020-02-07 07:44:48 -0700

    Adding camera support
    - The application on the RC car will now capture video from a camera
      and is controlled by a very simple configuration file.
    - Only captures in MJPEG at the moment, so the camera being used must
      support it and is assumed to output valid JPEG images. This will
      likely be addressed in the future.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use config;
+use rscam;
+use std::collections::HashMap;
+use std::io::prelude::*;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let mut settings = config::Config::default();
+    settings.merge(config::File::with_name("settings")).unwrap();
+    let settings_map = settings.try_into::<HashMap<String, String>>().unwrap();
+    let ip = settings_map.get("ip").unwrap().to_owned()
+        + ":"
+        + settings_map.get("port").unwrap();
+    let device = settings_map.get("device").unwrap();
+
+    let mut camera = rscam::new(device).expect("Cannot connect to camera");
+    camera
+        .start(&rscam::Config {
+            interval: (1, 30),
+            resolution: (1280, 720),
+            format: b"MJPG",
+            ..Default::default()
+        })
+        .expect("Cannot start camera");
+
+    let mut stream =
+        TcpStream::connect(ip).expect("Cannot connect to sensorview");
+
+    loop {
+        let frame = camera
+            .capture()
+            .expect("Unable to capture frame from camera");
+        let length = frame.len() as u32;
+        stream
+            .write_all(&length.to_le_bytes())
+            .expect("Couldn't write size");
+        stream.write_all(&frame[..]).expect("Couldn't write buffer");
+        stream.flush().expect("Couldn't flush TcpStream");
+    }
 }
