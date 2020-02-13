58a97a2864fae32532b333aec8423006e5e280cd
ostrosco <ostrosco@fastmail.fm>
2020-02-12 22:37:15 -0700

    Using OpenCV for video capture from the webcam
    - To improve stability of the video capture and support future computer
      vision work on the Pi, data from the webcam is now captured via
      OpenCV 4.2.
    - Added a Dockerfile that should (in theory) allow for
      cross-compilation. Currently an issue is filed against opencv-rust to
      address some issues with the build.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,57 @@
 use config;
-use rscam;
+use opencv::prelude::Vector;
+use opencv::{
+    core,
+    types::{VectorOfint, VectorOfuchar},
+    videoio,
+};
 use std::collections::HashMap;
+use std::error::Error;
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut settings = config::Config::default();
     settings.merge(config::File::with_name("settings")).unwrap();
     let settings_map = settings.try_into::<HashMap<String, String>>().unwrap();
     let ip = settings_map.get("ip").unwrap().to_owned()
         + ":"
         + settings_map.get("port").unwrap();
-    let device = settings_map.get("device").unwrap();
-
-    let mut camera = rscam::new(device).expect("Cannot connect to camera");
-    camera
-        .start(&rscam::Config {
-            interval: (1, 30),
-            resolution: (1280, 720),
-            format: b"MJPG",
-            ..Default::default()
-        })
-        .expect("Cannot start camera");
+    let mut cam = videoio::VideoCapture::new_with_backend(0, videoio::CAP_ANY)?;
+    cam.set(
+        videoio::CAP_PROP_FOURCC,
+        videoio::VideoWriter::fourcc(
+            'M' as i8, 'J' as i8, 'P' as i8, 'G' as i8,
+        )?
+        .into(),
+    )?;
+    let opened = videoio::VideoCapture::is_opened(&cam)?;
+    if !opened {
+        panic!("Unable to open default camera!");
+    }
 
     let mut stream =
         TcpStream::connect(ip).expect("Cannot connect to sensorview");
 
     loop {
-        let frame = camera
-            .capture()
-            .expect("Unable to capture frame from camera");
-        let length = frame.len() as u32;
-        stream
-            .write_all(&length.to_le_bytes())
-            .expect("Couldn't write size");
-        stream.write_all(&frame[..]).expect("Couldn't write buffer");
-        stream.flush().expect("Couldn't flush TcpStream");
+        let mut frame = core::Mat::default().expect("Can't make default frame");
+        let mut buf = VectorOfuchar::new();
+        cam.read(&mut frame)?;
+        if opencv::imgcodecs::imencode(
+            ".jpg",
+            &frame,
+            &mut buf,
+            &VectorOfint::new(),
+        )? {
+            let buf_slice = buf.to_slice();
+            let length = buf_slice.len();
+            stream
+                .write_all(&length.to_le_bytes())
+                .expect("Couldn't write size");
+            stream.write_all(buf_slice).expect("Couldn't write buffer");
+            stream.flush().expect("Couldn't flush TcpStream");
+        } else {
+            println!("Couldn't encode image");
+        }
     }
 }
